cfb1dcfbf9e40525ae1f6806de42ea99d3493163
Murtuza Quaizar <MQuaizar@RJBRANDS.local>
2022-11-06 12:07:40 -0500

    refactor window into class

--- pyQTcalculator.py
+++ pyQTcalculator.py
@@ -1,16 +1,34 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt6.QtWidgets import (
+    QApplication,
+    # QGridLayout,
+    # QLineEdit,
+    QMainWindow,
+    # QPushButton,
+    # QVBoxLayout,
+    QWidget, )
 
 WINDOW_SIZE = 235
 
 
+class pyQTCalWindow:
+    def __init__(self):
+        self.__window = QMainWindow()
+        self.__window.setWindowTitle("QT Calculator")
+        self.__window.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
+
+        centralWidget = QWidget()
+        self.__window.setCentralWidget(centralWidget)
+
+    def show(self):
+        self.__window.show()
+
+
 def main():
     app = QApplication([])
-    window = QMainWindow()
-    window.setWindowTitle("QT Calculator")
-    window.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
 
-    window.show()
+    cal_window = pyQTCalWindow()
+    cal_window.show()
 
     sys.exit(app.exec())
 
